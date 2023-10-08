df9912e763e8d790320ab5855dbd7e67edb806a7
vinay <bogivinaykumar@gmail.com>
2023-10-08 20:51:50 +0530

    sample haar transforms

---
+++ haar_transform.py
@@ -0,0 +1,17 @@
+
+
+class Haar_class:
+	
+	
+	i = 0
+	
+	def haar_transform(self, num):
+	
+		return "new class: " + str(num)
+
+if __name__ == '__main__':
+
+	X = Haar_class()	
+	print(X.haar_transform(1))
+	
+	
